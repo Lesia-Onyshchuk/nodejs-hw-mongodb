a00fc217413815aab909734a1655c026bc379ef5
lesia_onyshchuk <lkane443@gmail.com>
2025-04-01 00:05:02 +0300

    return

--- src/server.js
+++ src/server.js
@@ -37,6 +37,7 @@ export const setupServer = () => {
       res.status(404).json({
         message: 'Contact not found',
       });
+      return;
     }
     res.status(200).json({
       status: 200,
