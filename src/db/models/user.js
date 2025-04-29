8dff8b65a8d9e3001279438aee5296c0aa5c1c4b
lesia_onyshchuk <lkane443@gmail.com>
2025-04-29 13:58:03 +0300

    finish step 3

--- src/db/models/user.js
+++ src/db/models/user.js
@@ -9,4 +9,10 @@ const usersShema = new Schema(
   { timestamps: true, versionKey: false },
 );
 
+usersShema.methods.toJSON = function () {
+  const obj = this.toObject();
+  delete obj.password;
+  return obj;
+};
+
 export const UsersCollection = model('users', usersShema);
