233ee6b8f1cbdf21bbdd9caa2bfbb4b7546be851
lesia_onyshchuk <lkane443@gmail.com>
2025-04-29 19:03:09 +0300

    step 4

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -1,15 +1,24 @@
 import { Router } from 'express';
 import { validateBody } from '../middlewares/validateBody.js';
-import { registerUserShema } from '../validation/auth.js';
+import { loginUserSchema, registerUserSchema } from '../validation/auth.js';
 import { ctrlController } from '../utils/ctrlWrapper.js';
-import { registerUserController } from '../controllers/auth.js';
+import {
+  loginUserController,
+  registerUserController,
+} from '../controllers/auth.js';
 
 const router = Router();
 
 router.post(
   '/register',
-  validateBody(registerUserShema),
+  validateBody(registerUserSchema),
   ctrlController(registerUserController),
+);
+
+router.post(
+  '/login',
+  validateBody(loginUserSchema),
+  ctrlController(loginUserController),
 );
 
 export default router;
--- src/validation/auth.js
+++ src/validation/auth.js
@@ -1,7 +1,12 @@
 import Joi from 'joi';
 
-export const registerUserShema = Joi.object({
+export const registerUserSchema = Joi.object({
   name: Joi.string().min(3).max(30).required(),
   email: Joi.string().email().required(),
+  password: Joi.string().required(),
+});
+
+export const loginUserSchema = Joi.object({
+  email: Joi.string().min(3).max(30).required(),
   password: Joi.string().required(),
 });
